a37de136da10e1f839ca29844cd92757ecc6eb9c
Carlos Sala Samper <carlos15695@hotmail.com>
2017-11-25 20:19:15 +0100

    client commit

--- nazona/src/pages/home/home.ts
+++ nazona/src/pages/home/home.ts
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  //Atributos
+  viewMode: number = 0;
+  
   constructor(public navCtrl: NavController) {
+  }
+
+  /*
+  * Cambia el modo de vista de los resultados
+  */
+  toggleView(){
+    this.viewMode = (this.viewMode) ? 0 : 1;
+  }
+
+  /*
+  * Vista mapa
+  */
+  initMap(){
 
   }
+  isMapActive(){
+    return (this.viewMode == 0);
+  }
+
+  /*
+  * Vista lista
+  */
+  isListActive(){
+    return (this.viewMode == 1)
+  }
+
+  
 
 }
