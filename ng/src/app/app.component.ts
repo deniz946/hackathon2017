80be7a65f1f66df946d2bcffb64d8541654b4d0c
deniz946 <deniz946@gmail.com>
2017-11-25 22:46:50 +0100

    added leflet

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
+import { Map } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  options = {
+    layers: [
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: L.latLng([46.879966, -121.726909])
+  };
+
 }
